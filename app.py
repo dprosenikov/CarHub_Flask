d58b177912556301221708b0cece8821cfec1032
dprosenikov <dprosenikov86@gmail.com>
2021-12-12 22:13:46 +0200

    commit v1.2

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from os import environ
 
 from flask import Flask
 from flask_migrate import Migrate
@@ -9,7 +8,6 @@ from db import db
 from resources.cars import AllCars, MyCars
 from resources.users import RegisterUser, LoginUser
 
-
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -18,7 +16,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-
 migrate = Migrate(app, db)
 
 api = Api(app)
@@ -26,7 +23,6 @@ api.add_resource(RegisterUser, "/register")
 api.add_resource(LoginUser, "/login")
 api.add_resource(AllCars, "/allcars")
 api.add_resource(MyCars, "/mycars")
-
 
 if __name__ == "__main__":
     app.run(DEBUG=True)
